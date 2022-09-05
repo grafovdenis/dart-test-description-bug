86c9935ad7b6a1f95049e9e699290fe91db2e780
Denis Grafov <grafov.denis@gmail.com>
2022-09-05 16:17:14 +0300

    Initial commit

---
+++ bin/dart_test_description_bug.dart
@@ -0,0 +1,5 @@
+import 'package:dart_test_description_bug/dart_test_description_bug.dart' as dart_test_description_bug;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_test_description_bug.calculate()}!');
+}
---
+++ test/dart_test_description_bug_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_test_description_bug/dart_test_description_bug.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
